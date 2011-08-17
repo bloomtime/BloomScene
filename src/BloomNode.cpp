bac364fdea0d90eb95cba0665c53f5fba3a170e1
Tom Carden <tom@tom-carden.co.uk>
2011-08-16 22:04:40 -0700

    fix typo

--- src/BloomNode.cpp
+++ src/BloomNode.cpp
@@ -182,7 +182,7 @@ bool BloomNode::deepTouchEnded( TouchEvent::Touch touch )
             }
         }
         else {
-            consumed = nodeRef->deepTouchEnded( touch );
+            consumed = node->deepTouchEnded( touch );
         }
         mActiveTouches.erase(touch.getId());
     }
